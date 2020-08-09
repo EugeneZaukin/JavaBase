461f3e32f9ffbcae4fb04a1a602bfa021bd266bd
Eugene_Zaukin <Eugene_Zaukin@mail.ru>
2020-08-09 14:23:34 +0700

    Lesson7

--- src/Lesson_7/Main.java
+++ src/Lesson_7/Main.java
@@ -1,4 +1,16 @@
 package Lesson_7;
 
 public class Main {
+    public static void main(String[] args) {
+        Cat[] cat = {new Cat("Тимофей", 20), new Cat("Барсик",15),
+                new Cat("Кузя", 35), new Cat("Симба", 25),
+                new Cat("Том", 10)};
+        Plate plate = new Plate(100);
+
+        for (int i = 0; i < cat.length; i++) {
+            cat[i].info();
+            cat[i].eat(plate);
+            System.out.println();
+        }
+    }
 }
--- src/Lesson_7/Plate.java
+++ src/Lesson_7/Plate.java
@@ -1,5 +1,28 @@
 package Lesson_7;
 
 public class Plate {
+    private int food;
+
+    public Plate(int food) {
+        this.food = food;
+    }
+
+    public int getFood() {
+        return food;
+    }
+
+    public void info(){
+        System.out.println("Осталось еды: " + food);
+    }
+
+    public void decreaseFood(int amount){
+        if(food >= amount){
+            food = food - amount;
+        }
+    }
+
+    public void growthFood(int lot){
+         food = food + lot;
+    }
 
 }
